bda944488bcdc3d368f14ebadae3fee519aa257f
mq <313440254@qq.com>
2024-09-11 22:28:04 +0800

    初始化两个springboot项目

---
+++ spring-netty-demo/src/main/java/com/spring/netty/SpringNettyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNettyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNettyDemoApplication.class, args);
+    }
+
+}
---
+++ spring-transactional-demo/src/main/java/com/spring/transactional/SpringTransactionalDemoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.transactional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTransactionalDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTransactionalDemoApplication.class, args);
+    }
+
+}
